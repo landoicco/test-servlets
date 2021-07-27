430e5ae8a19e8bd9953aad6511c9b5ad103bf840
Lando Icaza <landoicco@gmail.com>
2021-07-27 14:13:38 -0500

    LogFilter now works fine!!

---
+++ src/main/java/web/LogFilter.java
@@ -0,0 +1,36 @@
+package web;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class LogFilter implements Filter {
+
+    Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        //Cast Servlet objects to HttpServlet objects
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+
+        String message = "REQUEST SEND-->" +  httpRequest.getMethod()
+                + " TO-->" + httpRequest.getRequestURI();
+
+        logger.info(message);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -5,19 +5,11 @@ boolean hasAccess = Boolean.parseBoolean((String)request.getAttribute("hasAccess
 if(!hasAccess){
   // Incluir context path para que encuentre el jsp
   response.sendRedirect(request.getContextPath() + "/index.html");
-}
-//RequestDispatcher headerRD = getServletContext().getRequestDispatcher("/html/header.html");
-//headerRD.include(request, response);
-//pageContext.include("/html/header.html");
+  }
 %>
 <%@include file="/WEB-INF/html/header.html"%>
 <body>
 <h3>Bienvenido</h3>
 <%@include file="/WEB-INF/html/footer.html"%>
-<%
-//RequestDispatcher footerRD = getServletContext().getRequestDispatcher("/html/footer.html");
-//footerRD.include(request, response);
-//pageContext.include("/html/footer.html");
-%>
 </body>
 </html>
