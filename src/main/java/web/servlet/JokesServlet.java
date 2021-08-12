8740c30256f704ce8de38827400fddc1fe8b4feb
Lando Icaza <landoicco@gmail.com>
2021-08-12 12:04:08 -0500

    Joke's HashMap working!!

--- src/main/java/web/servlet/JokesServlet.java
+++ src/main/java/web/servlet/JokesServlet.java
@@ -5,11 +5,12 @@ import jakarta.servlet.http.*;
 import local.jokes.Joke;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JokesServlet extends HttpServlet {
 
-    String jokeResponse;
-    Joke[] jokeBucket;
+    Map jokeBucket;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,55 +22,58 @@ public class JokesServlet extends HttpServlet {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
-        getJoke(req.getParameter("joke"));
-
-        //resp.getWriter().append(jokeResponse);
+        String jokeResponse = getJoke(req.getParameter("joke"));
         resp.getWriter().write(jokeResponse);
 
     }
 
-    private Joke[] createJokes() {
+    private Map createJokes() {
 
-        Joke jokeone = new Joke(1, "Whats the object-oriented way to become wealthy?",
+        final Joke jokeone = new Joke(1, "Whats the object-oriented way to become wealthy?",
                 "Inheritance");
-        Joke joketwo = new Joke(2, "What did the Java code say to the C code?",
+        final Joke joketwo = new Joke(2, "What did the Java code say to the C code?",
                 "You've got no class");
-        Joke jokethree = new Joke(3, "What do cats and programmers have in common?",
+        final Joke jokethree = new Joke(3, "What do cats and programmers have in common?",
                 "When either one is unusually happy and excited, an appropriate question would be, " +
                         "did you find a bug?");
-        Joke jokefour = new Joke(4, "What is the most used language in programming?",
+        final Joke jokefour = new Joke(4, "What is the most used language in programming?",
                 "Profanity");
 
-        return new Joke[]{jokeone, joketwo, jokethree, jokefour};
+        return new HashMap<Integer, Joke>()
+        {{
+            put(jokeone.getId(), jokeone);
+            put(joketwo.getId(), joketwo);
+            put(jokethree.getId(), jokethree);
+            put(jokefour.getId(), jokefour);
+        }};
 
     }
 
-    private void getJoke(String joke) {
+    private String getJoke(String joke) {
+
+        int requestedJoke = 0;
+
         switch (joke) {
             case "jokeone":
-                jokeResponse = "{\n" +
-                        "  \"Question\": " + "\"" + jokeBucket[0].getQuestion() + "\"" + ",\n" +
-                        "  \"Answer\" : " + "\"" + jokeBucket[0].getAnswer() + "\"" + "\n" +
-                        "}";
+                requestedJoke = 1;
                 break;
             case "joketwo":
-                jokeResponse = "{\n" +
-                        "  \"Question\": " + "\"" + jokeBucket[1].getQuestion() + "\"" + ",\n" +
-                        "  \"Answer\" : " + "\"" + jokeBucket[1].getAnswer() + "\"" + "\n" +
-                        "}";
+                requestedJoke = 2;
                 break;
             case "jokethree":
-                jokeResponse = "{\n" +
-                        "  \"Question\": " + "\"" + jokeBucket[2].getQuestion() + "\"" + ",\n" +
-                        "  \"Answer\" : " + "\"" + jokeBucket[2].getAnswer() + "\"" + "\n" +
-                        "}";
+                requestedJoke = 3;
                 break;
             case "jokefour":
-                jokeResponse = "{\n" +
-                        "  \"Question\": " + "\"" + jokeBucket[3].getQuestion() + "\"" + ",\n" +
-                        "  \"Answer\" : " + "\"" + jokeBucket[3].getAnswer() + "\"" + "\n" +
-                        "}";
+                requestedJoke = 4;
                 break;
         }
+
+        String question = ((Joke) jokeBucket.get(requestedJoke)).getQuestion();
+        String answer = ((Joke) jokeBucket.get(requestedJoke)).getAnswer();
+
+        return  "{\n" +
+                "  \"Question\": " + "\"" + question + "\"" + ",\n" +
+                "  \"Answer\" : " + "\"" + answer + "\"" + "\n" +
+                "}";
     }
 }
