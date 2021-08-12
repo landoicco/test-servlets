bf0dd0462a0c376a77366ecab28afc321c05bca4
Lando Icaza <landoicco@gmail.com>
2021-08-12 09:56:02 -0500

    Joke class working in array!

---
+++ src/main/java/local/jokes/Joke.java
@@ -0,0 +1,26 @@
+package local.jokes;
+
+public class Joke {
+
+    int id;
+    String question;
+    String answer;
+
+    public Joke(int id, String question, String answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
--- src/main/java/web/servlet/JokesServlet.java
+++ src/main/java/web/servlet/JokesServlet.java
@@ -2,15 +2,21 @@ package web.servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
+import local.jokes.Joke;
 
 import java.io.IOException;
 
 public class JokesServlet extends HttpServlet {
 
     String jokeResponse;
+    Joke[] jokeBucket;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (jokeBucket == null) {
+            jokeBucket = createJokes();
+        }
 
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
@@ -22,31 +28,46 @@ public class JokesServlet extends HttpServlet {
 
     }
 
+    private Joke[] createJokes() {
+
+        Joke jokeone = new Joke(1, "Whats the object-oriented way to become wealthy?",
+                "Inheritance");
+        Joke joketwo = new Joke(2, "What did the Java code say to the C code?",
+                "You've got no class");
+        Joke jokethree = new Joke(3, "What do cats and programmers have in common?",
+                "When either one is unusually happy and excited, an appropriate question would be, " +
+                        "did you find a bug?");
+        Joke jokefour = new Joke(4, "What is the most used language in programming?",
+                "Profanity");
+
+        return new Joke[]{jokeone, joketwo, jokethree, jokefour};
+
+    }
+
     private void getJoke(String joke) {
         switch (joke) {
             case "jokeone":
                 jokeResponse = "{\n" +
-                        "  \"Question\": \"Whats the object-oriented way to become wealthy?\",\n" +
-                        "  \"Answer\" : \"Inheritance\"\n" +
+                        "  \"Question\": " + "\"" + jokeBucket[0].getQuestion() + "\"" + ",\n" +
+                        "  \"Answer\" : " + "\"" + jokeBucket[0].getAnswer() + "\"" + "\n" +
                         "}";
                 break;
             case "joketwo":
                 jokeResponse = "{\n" +
-                        "  \"Question\": \"What did the Java code say to the C code?\",\n" +
-                        "  \"Answer\" : \"You've got no class\"\n" +
+                        "  \"Question\": " + "\"" + jokeBucket[1].getQuestion() + "\"" + ",\n" +
+                        "  \"Answer\" : " + "\"" + jokeBucket[1].getAnswer() + "\"" + "\n" +
                         "}";
                 break;
             case "jokethree":
                 jokeResponse = "{\n" +
-                        "  \"Question\": \"What do cats and programmers have in common?\",\n" +
-                        "  \"Answer\" : \"When either one is unusually happy and excited, an appropriate question would be," +
-                        "  did you find a bug?\"\n" +
+                        "  \"Question\": " + "\"" + jokeBucket[2].getQuestion() + "\"" + ",\n" +
+                        "  \"Answer\" : " + "\"" + jokeBucket[2].getAnswer() + "\"" + "\n" +
                         "}";
                 break;
             case "jokefour":
                 jokeResponse = "{\n" +
-                        "  \"Question\": \"What is the most used language in programming?\",\n" +
-                        "  \"Answer\" : \"Profanity\"\n" +
+                        "  \"Question\": " + "\"" + jokeBucket[3].getQuestion() + "\"" + ",\n" +
+                        "  \"Answer\" : " + "\"" + jokeBucket[3].getAnswer() + "\"" + "\n" +
                         "}";
                 break;
         }
