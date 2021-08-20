1a6a011e70afce61c7eea088fdd8bf48a5e03dcf
Lando Icaza <landoicco@gmail.com>
2021-08-20 08:50:18 -0500

    JokesServlet updated!!

--- src/main/java/local/jokes/Joke.java
+++ src/main/java/local/jokes/Joke.java
@@ -2,17 +2,17 @@ package local.jokes;
 
 public class Joke {
 
-    int id;
+    String id;
     String question;
     String answer;
 
-    public Joke(int id, String question, String answer) {
+    public Joke(String id, String question, String answer) {
         this.id = id;
         this.question = question;
         this.answer = answer;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
--- src/main/java/web/servlet/JokesServlet.java
+++ src/main/java/web/servlet/JokesServlet.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public class JokesServlet extends HttpServlet {
 
-    Map jokeBucket;
+    Map<String, Joke> jokeBucket;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,53 +27,36 @@ public class JokesServlet extends HttpServlet {
 
     }
 
-    private Map createJokes() {
+    private Map<String, Joke> createJokes() {
 
-        final Joke jokeone = new Joke(1, "Whats the object-oriented way to become wealthy?",
+        final Joke jokeone = new Joke("jokeone", "Whats the object-oriented way to become wealthy?",
                 "Inheritance");
-        final Joke joketwo = new Joke(2, "What did the Java code say to the C code?",
+        final Joke joketwo = new Joke("joketwo", "What did the Java code say to the C code?",
                 "You've got no class");
-        final Joke jokethree = new Joke(3, "What do cats and programmers have in common?",
+        final Joke jokethree = new Joke("jokethree", "What do cats and programmers have in common?",
                 "When either one is unusually happy and excited, an appropriate question would be, " +
                         "did you find a bug?");
-        final Joke jokefour = new Joke(4, "What is the most used language in programming?",
+        final Joke jokefour = new Joke("jokefour", "What is the most used language in programming?",
                 "Profanity");
+        final Joke nulljoke = new Joke("nulljoke", "That joke doesn't exist!", "Nice try, hacker!");
 
-        return new HashMap<Integer, Joke>()
-        {{
+        return new HashMap<String, Joke>() {{
             put(jokeone.getId(), jokeone);
             put(joketwo.getId(), joketwo);
             put(jokethree.getId(), jokethree);
             put(jokefour.getId(), jokefour);
+            put(nulljoke.getId(), nulljoke);
         }};
 
     }
 
     private String getJoke(String joke) {
 
-        int requestedJoke = 0;
+        Joke requestedJoke = jokeBucket.get(joke) != null ? (Joke) jokeBucket.get(joke) : (Joke) jokeBucket.get("nulljoke");
 
-        switch (joke) {
-            case "jokeone":
-                requestedJoke = 1;
-                break;
-            case "joketwo":
-                requestedJoke = 2;
-                break;
-            case "jokethree":
-                requestedJoke = 3;
-                break;
-            case "jokefour":
-                requestedJoke = 4;
-                break;
-        }
-
-        String question = ((Joke) jokeBucket.get(requestedJoke)).getQuestion();
-        String answer = ((Joke) jokeBucket.get(requestedJoke)).getAnswer();
-
-        return  "{\n" +
-                "  \"Question\": " + "\"" + question + "\"" + ",\n" +
-                "  \"Answer\" : " + "\"" + answer + "\"" + "\n" +
+        return "{\n" +
+                "  \"Question\": " + "\"" + requestedJoke.getQuestion() + "\"" + ",\n" +
+                "  \"Answer\" : " + "\"" + requestedJoke.getAnswer() + "\"" + "\n" +
                 "}";
     }
 }
