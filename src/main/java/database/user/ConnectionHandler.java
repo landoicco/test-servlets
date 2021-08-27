92662822672fe6ecdc29f07a54c4aff5aca5979e
Lando Icaza <landoicco@gmail.com>
2021-08-27 11:39:50 -0500

    DB Connection Pool working!!

--- src/main/java/database/user/ConnectionHandler.java
+++ src/main/java/database/user/ConnectionHandler.java
@@ -1,5 +1,8 @@
 package database.user;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class ConnectionHandler {
@@ -16,8 +19,20 @@ public class ConnectionHandler {
         }
     }
 
+    public static DataSource getDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USER);
+        ds.setPassword(JDBC_PASS);
+
+        // Definimos la cantidad de conexiones disponibles
+        ds.setInitialSize(5);
+
+        return ds;
+    }
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+        return getDataSource().getConnection();
     }
 
     public static void close(ResultSet rs) {
